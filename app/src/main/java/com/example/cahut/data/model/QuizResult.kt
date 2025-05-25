ff347a10181d01d660a0af62b74feddae44f5093
hoan23a2 <21021590@vnu.edu.vn>
2025-05-25 16:59:19 +0700

    sửa ui bxh

--- app/src/main/java/com/example/cahut/data/model/QuizResult.kt
+++
@@ -1,8 +0,0 @@
-// XÓA: data class LeaderboardEntry(
-// XÓA:     val id: String,
-// XÓA:     val rank: Int,
-// XÓA:     val username: String,
-// XÓA:     val score: Int,
-// XÓA:     val userImage: Int = 1,
-// XÓA:     val isCorrectForLastQuestion: Boolean = false
-// XÓA: ) 
